8c6d24a86838b162b128081e1fef589aedc5f727
SaschaKohler <sascha.kohler@sent.at>
2022-01-30 09:21:39 +0100

    user policies backend & driving book

--- config/admin.php
+++ config/admin.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'url' => env('ADMIN_URL', 'http://localhost:8080'),
+    'url' => env('ADMIN_URL', 'https://dirneder-app.herokuapp.com:8080'),
     'read_only' => env('ADMIN_READ_ONLY', false),
 ];
