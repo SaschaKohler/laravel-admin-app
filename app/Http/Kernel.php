bebc9a4e967c92f9d9026fda29d53e600389f70d
SaschaKohler <sascha.kohler@sent.at>
2022-01-31 14:23:46 +0100

    minus are stateful

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,7 +40,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Okami101\LaravelAdmin\Http\Middleware\Impersonate::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
