f4f935f786dee6305dcaf7608660fdd9cf60a871
SaschaKohler <sascha.kohler@sent.at>
2022-02-22 06:58:49 +0100

    timed -> ganzt / localized email

--- database/seeders/EventSeeder.php
+++ database/seeders/EventSeeder.php
@@ -29,7 +29,7 @@ class EventSeeder extends Seeder
         $users = User::all();
         $tools = Tool::all();
 
-        Event::factory(10)->make()->each(function (Event $event) use ($customers, $vehicles, $users, $tools) {
+        Event::factory(300)->make()->each(function (Event $event) use ($customers, $vehicles, $users, $tools) {
             $event->customer()->associate($customers->random());
 
             $event->color = $this->colors[random_int(0, 6)];
