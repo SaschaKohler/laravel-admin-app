2ed3155e6dab8b59652d044cdbb6948135969920
SaschaKohler <sascha.kohler@sent.at>
2022-03-20 21:02:32 +0100

    user password change

--- admin/src/resources/users/Form.vue
+++ admin/src/resources/users/Form.vue
@@ -5,8 +5,6 @@
         <va-text-input source="phone1" required></va-text-input>
         <va-text-input source="address"></va-text-input>
         <va-boolean-input source="active"></va-boolean-input>
-        <va-password-input source="password"></va-password-input>
-        <va-password-input source="password_confirmation"></va-password-input>
         <va-select-input
             source="roles"
             multiple
